368c0a40219390a15c00adcd17fff0036f9723e6
RenaldoJameson <67342328+yelluctrebor@users.noreply.github.com>
2022-12-05 21:03:47 -0700

    add button class

---
+++ button.py
@@ -0,0 +1,34 @@
+import pygame
+
+# button class
+#https://www.youtube.com/watch?v=2iyx8_elcYg&t=331s
+
+
+class Button():
+    def __init__(self, x_pos, y_pos, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(
+            image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x_pos, y_pos)
+        self.clicked = False
+
+    def draw(self, surface):
+        action = False
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        # draw button on screen
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
